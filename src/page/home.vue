a29a5f5ee5f49dacb15824476d7a8cc2b62f02ed
gaoming <1020876268@qq.com>
2020-09-01 13:38:03 +0800

    1.0.0

---
+++ src/page/home.vue
@@ -0,0 +1,34 @@
+<template>
+    <div class="pdf-demo">
+
+        <button @click="handleDown">jsPDF方式下载</button>
+        <button @click="handleWindowPrint( '#demo', '电子合同' )">浏览器方式下载</button>
+
+        <div id="demo" >
+                #demo 中的内容导出成 PDF
+        </div>
+
+
+    </div>
+
+</template>
+
+<script>
+import htmlToPdf from '@/assets/js/htmlToPdf';
+export default {
+  data () {
+    return {
+      name: 'hello'
+    }
+  },
+   methods: {
+
+            handleDown(){
+                //导出PDF
+                htmlToPdf.downloadPDF( document.querySelector('#demo'),'我的PDF');
+            },
+
+
+        }
+}
+</script>
